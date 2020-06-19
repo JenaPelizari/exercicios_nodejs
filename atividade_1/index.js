76e34dbeff4dee05dbf747e960c9e390a5a25d06
JenaPelizari <jn.up@hotmail.com>
2020-06-19 20:25:52 -0300

    primeiro exercicio resolvido

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -1,5 +1,16 @@
 // ## Importe a biblioteca ##
 
-
+let rs = require('readline-sync')
 
 // ## Faça o código ##
+
+let numVoo = rs.question('Qual é o numero do vôo?')
+
+let fileira = rs.question('Em qual fileira você deseja sentar?')
+
+console.log('Cartão de embarque para o vôo de número:' +numVoo)
+
+console.log('Você sentará na fileira:' +fileira)
+
+
+
