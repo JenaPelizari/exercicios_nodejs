12d8d7a06164d2ecc896c22f74a1698403b432a3
JenaPelizari <jn.up@hotmail.com>
2020-06-19 21:21:19 -0300

    atividade 3 pronta

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,11 @@
 // ## Importe a biblioteca ##
 
-
+let rs = require ('readline-sync')
 
 // ## Faça o código ##
+
+let tamanho = rs.question ('Qual é o tamanho dos lados do quadrado?')
+
+let area = tamanho * tamanho
+
+console.log ('A área do seu quadrado é' + area + 'cm')
